4acb0022a72bc78ec10f2ce9247c0ba644e5d604
eiqballl <iqbalnurriz@gmail.com>
2026-09-03 13:29:11 +0800

    push pertama

---
+++ Documents/NetBeansProjects/PraktikumDua/src/main/java/com/mycompany/praktikumdua/PraktikumDua.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.praktikumdua;
+
+/**
+ *
+ * @author Acer
+ */
+public class PraktikumDua {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
