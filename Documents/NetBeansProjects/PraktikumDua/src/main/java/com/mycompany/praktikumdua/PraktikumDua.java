b6eaad5878662531d881aa3edd278d077327ed89
eiqballl <iqbalnurriz@gmail.com>
2026-09-03 13:45:03 +0800

    push kedua

--- Documents/NetBeansProjects/PraktikumDua/src/main/java/com/mycompany/praktikumdua/PraktikumDua.java
+++ Documents/NetBeansProjects/PraktikumDua/src/main/java/com/mycompany/praktikumdua/PraktikumDua.java
@@ -11,6 +11,6 @@ package com.mycompany.praktikumdua;
 public class PraktikumDua {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Haiiiii!");
     }
 }
